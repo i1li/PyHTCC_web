fcd8ed0cee75dad2a62eeada1955e3740063e610
i1li <74943880+i1li@users.noreply.github.com>
2024-10-29 16:51:12 -0500

    i

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Limitations Addressed: The Honeywell app only alows 1 saved schedule, 4 schedule
 - Unlimited schedule timeslots with 1 minute increments
 - Save & choose from multiple schedules
 - Import & export saved schedules
-- Choose to follow the set schedule, temporarily hold a temperature, or permanently hold
-- [Hysteresis (AKA deadband)](#what-is-hysteresis)
+- Choose to follow the set schedule, temporarily hold a temperature, or hold permanently
+- [Hysteresis (AKA deadband)](#what-is-hysteresis) - Adds optional temerature tolerance to reduce frequent on/off cycles.
 
 ## Getting Started
 
@@ -27,12 +27,18 @@ Run `pyhtcc_web.py`, then open a browser window to `localhost:5001`. To load sam
 ## What is Hysteresis?
 <a href="https://search.brave.com/search?q=hvac+deadband+hysteresis&source=web&summary=1&summary_og=391a2b9ee4a6faf7cb0377">Hysteresis (AKA deadband)</a> is the amount of degrees away from the setpoint allowed between active & rest cycles.
 
-Active Hysteresis is the amount of degrees beyond setpoint system run to before switching to rest.
+Active Hysteresis is the amount of degrees beyond setpoint system run to before switching to rest. A clearer term for this might be "Run Cycle Tolerance"
 
-Passive Hysteresis is the amount of degrees beyond setpoint system rests before switching on again.
+Passive Hysteresis is the amount of degrees beyond setpoint system rests before switching on again. A clearer term for this might be "Rest Cycle Tolerance"
 
 - Optional fields: leave blank or `0` for default behavior.
 - Useful to reduce frequent on/off cycles, especially when the output of the unit is high relative to the space controlled.
 
+Typical hysteresis doesn't differentiate between active & passive, splitting the hysteresis setting evenly in both directions of setpoint. (Setting the same value for active & passive will give this result.)
+
 ### State Transitions
-The transitions in the `hysteresis` function are based on updates of `running` and whether it is switching states while reaching or departing from the temp limit ranges based on heat/cool mode, and rest/running state.
+The transitions in the `hysteresis` function are based on updates received from the thermostat of current temp and running status, and whether reaching or departing from the temp limit ranges based on heat/cool mode, and rest/running state.
+
+Example: An Active Hysteresis at 1 and Passive Hysteresis at 2, in cool mode, set at 72 degrees, will cool until 71, then enter rest until 74.
+
+In heat mode this same setting will heat until 73, then enter rest until 70.
--- static/hysteresis.js
+++ static/hysteresis.js
@@ -2,36 +2,36 @@ function hysteresis(rawSetpoint, mode) {
     AC.rawSetpoint = rawSetpoint;
     const now = Date.now();
     const isCooling = mode === 'cool';
-    AC.restSetpoint = isCooling ? AC.rawSetpoint + AC.passiveHysteresis : AC.rawSetpoint - AC.passiveHysteresis;
     AC.activeSetpoint = isCooling ? AC.rawSetpoint - AC.activeHysteresis : AC.rawSetpoint + AC.activeHysteresis;
-    const isInRestRange = isCooling ? () => AC.currentTemp >= AC.rawSetpoint - AC.activeHysteresis : () => AC.currentTemp <= AC.rawSetpoint + AC.activeHysteresis;
     const isAtActiveSetpoint = AC.currentTemp === AC.activeSetpoint;
+    AC.runningFor = AC.runningSince ? now - AC.runningSince : 0;
+    AC.runningAtEdgeFor = AC.runningAtEdgeSince ? now - AC.runningAtEdgeSince : 0;
+    AC.restSetpoint = isCooling ? AC.rawSetpoint + AC.passiveHysteresis : AC.rawSetpoint - AC.passiveHysteresis;
     const isAtRestSetpoint = AC.currentTemp === AC.restSetpoint;
-    AC.restingDuration = AC.restingSince ? now - AC.restingSince : 0;
-    AC.restingAtLimitDuration = AC.restingAtLimitSince ? now - AC.restingAtLimitSince : 0;
-    AC.runningDuration = AC.runningSince ? now - AC.runningSince : 0;
-    AC.runningAtLimitDuration = AC.runningAtLimitSince ? now - AC.runningAtLimitSince : 0;
+    AC.restingFor = AC.restingSince ? now - AC.restingSince : 0;
+    AC.restingAtEdgeFor = AC.restingAtEdgeSince ? now - AC.restingAtEdgeSince : 0;
+    const isInRestRange = isCooling ? () => AC.currentTemp >= AC.rawSetpoint - AC.activeHysteresis : () => AC.currentTemp <= AC.rawSetpoint + AC.activeHysteresis;
     if (AC.running) {
         AC.resting = false;
         AC.restingSince = null;
-        AC.restingAtLimitSince = null;
+        AC.restingAtEdgeSince = null;
         if (!AC.runningSince) {
             AC.runningSince = now;
         }
         if (isAtActiveSetpoint) {
-            if (!AC.runningAtLimitSince) {
-                AC.runningAtLimitSince = now;
+            if (!AC.runningAtEdgeSince) {
+                AC.runningAtEdgeSince = now;
             }
-            if (AC.runningAtLimitDuration >= AC.atLimitMinTime) {
+            if (AC.runningAtEdgeFor >= AC.atEdgeMinTime) {
                 AC.readyToRest = true;
             }
         } else {
             AC.readyToRest = false;
-            AC.runningAtLimitSince = null;
+            AC.runningAtEdgeSince = null;
         }
     } else { //running=false
         AC.runningSince = null;
-        AC.runningAtLimitSince = null;
+        AC.runningAtEdgeSince = null;
         if (isInRestRange()) {
             if (isAtActiveSetpoint) {
                 if (AC.readyToRest) {
@@ -39,26 +39,26 @@ function hysteresis(rawSetpoint, mode) {
                     if (!AC.restingSince) {
                         AC.restingSince = now;
                     }
-                    if (!AC.restingAtLimitSince) {
-                        AC.restingAtLimitSince = now;
+                    if (!AC.restingAtEdgeSince) {
+                        AC.restingAtEdgeSince = now;
                     }
                 }
             } else { // isAtActiveSetpoint=false
                 AC.readyToRest = false;
-                AC.restingAtLimitSince = null;
+                AC.restingAtEdgeSince = null;
             }
         } else { // isInRestRange=false
             AC.resting = false;
             AC.restingSince = null;
-            AC.restingAtLimitSince = null;
+            AC.restingAtEdgeSince = null;
             AC.readyToRest = false;
         }
     }
-    AC.setpointToUse = AC.resting ? AC.restSetpoint : AC.activeSetpoint;
-    setThermostat(AC.setpointToUse, mode);
+    AC.hysteresisSetpoint = AC.resting ? AC.restSetpoint : AC.activeSetpoint;
+    setThermostat(AC.hysteresisSetpoint, mode);
 }
 function setThermostat(setpoint, mode) {
-    if (mode !== AC.currentMode || AC.setpointToUse !== AC.currentSetpoint) {
-        $.post('/set_update', { mode: mode, setpoint: AC.setpointToUse });
+    if (mode !== AC.currentMode || AC.hysteresisSetpoint !== AC.currentSetpoint) {
+        $.post('/set_update', { mode: mode, setpoint: AC.hysteresisSetpoint });
     }
 }
